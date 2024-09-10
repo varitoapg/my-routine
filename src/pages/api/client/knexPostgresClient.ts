267bdcb043655f2e461e932f2fafba2e36844516
Alvaro Parada <alvaro.parada92@gmail.com>
2024-09-10 16:08:16 +0200

    Move knex client to pages

--- src/pages/api/client/knexPostgresClient.ts
+++ src/pages/api/client/knexPostgresClient.ts
@@ -1,5 +1,7 @@
+import { Knex } from "knex";
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-export const knexPostgresClient = require("knex")({
+export const knexPostgresClient: Knex = require("knex")({
   client: "pg",
   connection: process.env.POSTGRES_URL,
   searchPath: ["knex", "public"],
