56b7f8763d745b6e7eb2731374190024b0a4b85c
Alvaro Parada <alvaro.parada92@gmail.com>
2024-09-13 14:02:38 +0200

    Update tailwind config with used colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
     fontFamily: {
       main: ["Roboto", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          green: "#7AC74F", // buttons
+          "green-hover": "#67A83E",
+          orange: "#FFB74D", // call to actions, icons, buttons...
+          "orange-hover": "#E69A30",
+        },
+        secondary: {
+          cream: "#FDF3E7",
+          blue: "#4F83CC", // Good for links and text accents
+        },
+        accent: {
+          red: "#FF6347",
+          "red-hover": "#E65239",
+          yellow: "#FFEB3B",
+          "yellow-hover": "#E4C92F",
+        },
+        text: {
+          darkSlate: "#2C3E50",
+          charcoalGray: "#37474F",
+        },
+      },
+    },
   },
   plugins: [],
 };
