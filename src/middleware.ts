41c594a85fa15aac0edc8f75967960d78872710f
Alvaro Parada <alvaro.parada92@gmail.com>
2024-09-17 15:34:40 +0200

    Update middleware to be more reusable

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { verifyToken } from "lib/auth/verifyToken";
 
+const protectedPaths = ["/my-routine"];
+
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("auth_token")?.value;
   const isAuthenticated = await verifyToken(token);
 
   const url = request.nextUrl.clone();
 
-  if (!isAuthenticated && url.pathname.startsWith("/my-routine")) {
+  const shouldProtect = protectedPaths.some((path) =>
+    url.pathname.startsWith(path),
+  );
+
+  if (!isAuthenticated && shouldProtect) {
     url.pathname = "/login";
     return NextResponse.redirect(url);
   }
@@ -16,5 +22,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/my-routine/:path*"],
+  matcher: ["/:path*"],
 };
