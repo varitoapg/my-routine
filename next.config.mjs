a63885d2a30af0e4bdd9bb4615d8a2bec3d13e7a
Alvaro Parada <alvaro.parada92@gmail.com>
2024-09-26 16:29:57 +0200

    Remove CORS configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  crossOrigin: "anonymous", // Use 'anonymous' or 'use-credentials' based on your needs
-  async headers() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://your-frontend-origin.com",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
