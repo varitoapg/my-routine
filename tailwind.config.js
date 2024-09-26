f184c6f7bfc88747fbe481f343524f6879571996
Alvaro Parada <alvaro.parada92@gmail.com>
2024-09-26 14:03:13 +0200

    Install and configure storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./stories/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       main: ["Roboto", "sans-serif"],
