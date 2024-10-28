aa63bcf116cf247e1bd2f680799d0eab95b213ec
Alvaro Parada <alvaro.parada92@gmail.com>
2024-10-28 17:20:35 +0100

    Implement validation in register form

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { LoginUser } from "@api/types/user";
+import Link from "next/link";
+import { Formik, Form, Field } from "formik";
 import { useRegister } from "@hooks/useRegister/useRegister";
 import Button from "@components/UI/Button/Button";
 import { Alert } from "@components/UI/Alert/Alert";
+import TextInput from "@components/Form/TextInput/TextInput";
+import PasswordInput from "@components/Form/PasswordInput/PasswordInput";
 import { TFunction } from "i18next";
+import { getRegisterSchema } from "./RegisterValidation";
+import { LoginUser } from "@api/types/user";
 
 interface RegisterFormProps {
   t: TFunction;
 }
+
 const RegisterForm = ({ t }: RegisterFormProps) => {
   const router = useRouter();
 
@@ -19,92 +23,67 @@ const RegisterForm = ({ t }: RegisterFormProps) => {
 
   const { mutate, isPending } = useRegister(redirectToRoutine);
 
-  const initialLogindFormData: LoginUser = {
-    username: "",
-    password: "",
-  };
-
-  const [formLoginData, setFormLoginData] = useState<LoginUser>(
-    initialLogindFormData,
-  );
-
-  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFormLoginData({
-      ...formLoginData,
-      [event.target.id]: event.target.value,
-    });
-  };
-  const handleSubmit = async (event: React.SyntheticEvent) => {
-    event.preventDefault();
-
+  const handleSubmit = async (values: LoginUser) => {
     try {
-      await mutate(formLoginData);
+      await mutate(values);
     } catch (error) {
       console.error("Error while submitting form: ", error);
     }
   };
-  return (
-    <>
-      <div className="w-full max-w-md">
-        <h2 className="mb-6 text-center text-3xl font-semibold text-gray-800">
-          {t("registerTitle")}
-        </h2>
-        <form className="space-y-6" onSubmit={handleSubmit}>
-          <div>
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium text-gray-700"
-            >
-              {t("username")}
-            </label>
-            <input
-              onChange={handleFormChange}
-              id="username"
-              name="username"
-              type="username"
-              autoComplete="username"
-              required
-              className="mt-1 block w-full rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-primary-green focus:outline-none focus:ring-primary-green sm:text-sm"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              {t("password")}
-            </label>
-            <input
-              onChange={handleFormChange}
-              id="password"
-              name="password"
-              type="password"
-              autoComplete="current-password"
-              required
-              className="mt-1 block w-full rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-primary-green focus:outline-none focus:ring-primary-green sm:text-sm"
-            />
-          </div>
 
-          <div className="flex flex-row gap-2">
-            <p>{t("haveAccount")}</p>
-            <Link
-              href="/login"
-              className="text-secondary-blue transition-transform duration-300 hover:scale-105 hover:text-secondary-blue-hover"
-            >
-              {t("login")}
-            </Link>
-          </div>
-          <div>
-            <Button fullWidth={true} disabled={isPending}>
-              {t("signUp")}
-            </Button>
-          </div>
-        </form>
-        <Alert variant="warning" showIcon className="mt-5">
-          <p>{t("registerWarning")}</p>
-        </Alert>
-      </div>
-    </>
+  return (
+    <div className="w-full max-w-md">
+      <h2 className="mb-6 text-center text-3xl font-semibold text-gray-800">
+        {t("registerTitle")}
+      </h2>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validationSchema={getRegisterSchema(t)}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting, isValid }) => (
+          <Form className="space-y-6">
+            <Field
+              name="username"
+              id="username"
+              label={t("username")}
+              component={TextInput}
+              required
+            />
+
+            <Field
+              name="password"
+              id="password"
+              label={t("password")}
+              component={PasswordInput}
+              required
+            />
+
+            <div className="flex flex-row gap-2">
+              <p>{t("haveAccount")}</p>
+              <Link
+                href="/login"
+                className="text-secondary-blue transition-transform duration-300 hover:scale-105 hover:text-secondary-blue-hover"
+              >
+                {t("login")}
+              </Link>
+            </div>
+            <div>
+              <Button
+                fullWidth={true}
+                disabled={isPending || isSubmitting || !isValid}
+                type="submit"
+              >
+                {t("signUp")}
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+      <Alert variant="warning" showIcon className="mt-5">
+        <p>{t("registerWarning")}</p>
+      </Alert>
+    </div>
   );
 };
 
