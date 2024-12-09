9167a74446dd6d01c2b808ca86e9af0bd98e3c60
Alvaro Parada <alvaro.parada92@gmail.com>
2024-12-09 14:13:49 +0100

    Add query to add a new ingredient

---
+++ src/pages/api/v1/ingredient/index.ts
@@ -0,0 +1,75 @@
+import { knexPostgresClient } from "@api/client/knexPostgresClient";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Ingredients } from "@api/types/typesFromDB";
+import { sendError } from "@api/utils/responses";
+import {
+  AppError,
+  Conflict,
+  InternalError,
+  MethodNotAllowed,
+} from "@lib/errors/AppError";
+import { checkAndDecodeToken } from "@api/utils/checkAndDecodeToken";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    const { method } = req;
+
+    if (method === "POST") {
+      await post(req, res);
+    } else {
+      res.setHeader("Allow", ["POST"]);
+      throw new MethodNotAllowed("Method not allowed");
+    }
+  } catch (error: unknown) {
+    if (error instanceof AppError) {
+      sendError(res, error);
+      return;
+    }
+    sendError(res, new InternalError());
+  }
+}
+
+async function post(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { id_measure, name } = req.body as Ingredients;
+    const userData = await checkAndDecodeToken(req);
+
+    const ingredient: Ingredients = await knexPostgresClient("ingredients")
+      .select("*")
+      .where({ name })
+      .andWhere(function () {
+        this.where("id_user", userData.userId).orWhere(function () {
+          this.whereNotNull("id_group").andWhere("id_group", userData.groupId);
+        });
+      })
+      .first();
+
+    if (ingredient) {
+      throw new Conflict("Ingredient already exists", "ingredient");
+    }
+
+    const newIngredient = await knexPostgresClient("ingredients")
+      .insert({
+        name,
+        id_measure,
+        id_user: userData.userId,
+        id_group: userData.groupId,
+      })
+      .returning(["ingredient_id", "name"]);
+
+    res.status(201).json({
+      success: true,
+      message: "Ingredient added successfully",
+      data: newIngredient[0],
+    });
+  } catch (error: unknown) {
+    if (error instanceof AppError) {
+      sendError(res, error);
+      return;
+    }
+    sendError(res, new InternalError());
+  }
+}
